3faff65292d082f42fab925cd787a76678d006de
An Nguyen <annguyenmxh@gmail.com>
2024-10-20 11:47:41 +0700

    12.4 50%

--- src/component/Role/Role.js
+++ src/component/Role/Role.js
@@ -4,28 +4,33 @@ import { FaTrashCan } from 'react-icons/fa6';
 import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import _ from 'lodash';
+import { toast } from 'react-toastify';
 const Roles = () => {
+    const dataChildDefault = {
+        url: '',
+        description: '',
+        isValidUrl: true,
+    };
     const [listChilds, setListChilds] = useState({
-        child1: { url: '', description: '' },
+        child1: dataChildDefault,
     });
     useEffect(() => {
         Object.entries(listChilds).map(([key, value]) => {
-            console.log(key, value);
-            // Pretty straightforward - use key for the key and value for the value.
-            // Just to clarify: unlike object destructuring, the parameter names don't matter here.
+            console.log('checkhihihi', key, value);
         });
-    }, []);
+    }, [listChilds]); // Thêm listChilds vào dependency array
+
     const handleOnchangeInput = (name, value, key) => {
         let _listChilds = _.cloneDeep(listChilds);
         _listChilds[key][name] = value;
+        if (value && name === 'url') {
+            _listChilds[key]['isValidUrl'] = true;
+        }
         setListChilds(_listChilds);
     };
     const handleAddNewInput = () => {
         let _listChilds = _.cloneDeep(listChilds);
-        _listChilds[`child-${uuidv4()}`] = {
-            url: '',
-            description: '',
-        };
+        _listChilds[`child-${uuidv4()}`] = dataChildDefault;
         setListChilds(_listChilds);
     };
     const handleDeleteInput = (key) => {
@@ -33,6 +38,23 @@ const Roles = () => {
         delete _listChilds[key];
         setListChilds(_listChilds);
     };
+    const handleSave = () => {
+        let _listChilds = _.cloneDeep(listChilds);
+        let check = true;
+        Object.entries(_listChilds).forEach(([key, child]) => {
+            if (!child.url) {
+                child.isValidUrl = false;
+                check = false;
+            }
+        });
+        if (check) {
+            //call
+        } else {
+            toast.error("Input URL Must Not Be Empty...")
+            setListChilds(_listChilds);
+        }
+    };
+
     return (
         <div className="role-container">
             <div className="container">
@@ -48,7 +70,7 @@ const Roles = () => {
                                         <label>URL:</label>
                                         <input
                                             type="text"
-                                            className="form-control"
+                                            className={child.isValidUrl ? 'form-control' : 'form-control is-invalid'}
                                             value={child.url}
                                             onChange={(event) => handleOnchangeInput('url', event.target.value, key)}
                                         />
@@ -76,7 +98,9 @@ const Roles = () => {
                             );
                         })}
                         <div>
-                            <button className="btn btn-warning mt-3">save</button>
+                            <button className="btn btn-warning mt-3" onClick={() => handleSave()}>
+                                save
+                            </button>
                         </div>
                     </div>
                 </div>
